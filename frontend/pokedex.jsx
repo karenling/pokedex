69ec4f5dd4f3fa83e9a513542943181014259a28
Karen Ling <karensling@gmail.com>
2016-06-01 21:53:07 -0700

    Set up react router.

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var HashHistory = require('react-router').hashHistory;
 var PokemonsIndex = require('./components/pokemons/pokemonsIndex');
+var App = require('./components/app');
+
+var routes = (
+  <Route path="/" component={App}></Route>
+);
 
 document.addEventListener("DOMContentLoaded", function() {
-  ReactDOM.render(<PokemonsIndex />, document.getElementById('root'));
+  ReactDOM.render(<Router history={ HashHistory }>{ routes }</Router>, document.getElementById('root'));
 });
